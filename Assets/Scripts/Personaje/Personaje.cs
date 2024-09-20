aaaceef35e225bee3e42a9bebc3e58fe6c20fa94
Giancarlo Oscar <20193201@aloe.ulima.edu.pe>
2024-09-20 18:50:45 -0500

    MIAU

--- Assets/Scripts/Personaje/Personaje.cs
+++ Assets/Scripts/Personaje/Personaje.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//miau
 public class Personaje :MonoBehaviour
 {
     public PersonajeVida PersonajeVida {get; private set;}
