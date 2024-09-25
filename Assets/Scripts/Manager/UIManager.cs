2b56d45a21a35fea5ac5bde520652ffb11f9e8ac
Giancarlo Oscar <20193201@aloe.ulima.edu.pe>
2024-09-25 01:34:20 -0500

    INVENTARIO COMPLETADO Y DISEÑOS NUEVOS

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -10,6 +10,7 @@ public class UIManager : Singleton<UIManager>
     [Header("Config")]
     [SerializeField] private Image vidaPlayer;
     [SerializeField] private TextMeshProUGUI vidaTMP;
+    [SerializeField] private GameObject panelInventario;
     // Start is called before the first frame update
     private float vidaActual;
     private float vidaMax;
@@ -29,5 +30,9 @@ public class UIManager : Singleton<UIManager>
         vidaPlayer.fillAmount=Mathf.Lerp(vidaPlayer.fillAmount,
         vidaActual/vidaMax, 10f * Time.deltaTime);
         vidaTMP.text=$"{vidaActual}/{vidaMax}";
+    }
+    public void abrirCerrarPanelInventario()
+    {
+        panelInventario.SetActive(!panelInventario.activeSelf);
     }
 }
